1160167eeaff69cefde9d4a27c02ddd89ac26963
DARKNAGAN <ccunatbrule@gmail.com>
2019-02-06 16:07:51 +0100

    CV Change
    Amélioration des dimensions des images pour la responsivité
    Mise au propre du code CSS et CV
    Fin des modifications CV

--- ChristianPortfolio/vue/cv.php
+++ ChristianPortfolio/vue/cv.php
@@ -1,184 +1,167 @@
-		<div id="cv" class="container-fluid bg-1 ">	<!-- Container (CV Section) -->
-			<br><br><center><h1 class="">CURRICULUM VITAE</h1></center><br>			
+		<div id="cv" class="container-fluid bg-1 "><br><br>
+			<h1 class="w3-center">CURRICULUM VITAE</h1><br>			
 			<div class="w3-content w3-margin-top" style="max-width:1400px;">	<!-- Page Container -->	
 				<div class="w3-row-padding">	<!-- The Grid -->				
 					<div class="w3-third">	<!-- Left Column -->
 						<div class="w3-container w3-white w3-text-grey w3-card-4">
-								<center><h1>Christian<br>CUNAT-BRULÉ</h1></center><hr>
-								<center><h5><b>Ingénieur développeur informatique</b></h5></center><hr>
-								<p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Etudiant / Informaticien</p>
-								<p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Essonne, France</p>
-								<p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>07.**.**.36.34</p>
-								<p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>ccunatbrule@gmail.com</p>
-								<p><i class="fa fa-at fa-fw w3-margin-right w3-large w3-text-teal"></i>ccunatbrule.free.fr</p>
-								<p><i class="w3-margin-right w3-large w3-text-teal"></i>22 ans - Permis - Véhicule</p>
+							<h1 class="w3-center">Christian<br>CUNAT-BRULÉ</h1><hr>
+							<h5 class="w3-center"><b>Ingénieur développeur informatique</b></h5><hr>
+							<p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Etudiant / Informaticien</p>
+							<p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Essonne, France</p>
+							<p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>07.**.**.36.34</p>
+							<p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>ccunatbrule@gmail.com</p>
+							<p><i class="fa fa-at fa-fw w3-margin-right w3-large w3-text-teal"></i>ccunatbrule.free.fr</p>
+							<p><i class="w3-margin-right w3-large w3-text-teal"></i>22 ans - Permis - Véhicule</p>
 						</div><hr>
 						<div class="w3-container w3-white w3-text-grey w3-card-4">
-								<div class="grid-container">
-									<p>Autonome <br> <span class="fa fa-star checked"></span></p>
-									<p></p>
-									<p>Confirmé <br><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
-								</div>
-								<p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Langues</b></p>
-								<div class="grid-container">
-									<p>Français</p>
-									<p></p>
-									<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
-									<p> Anglais</p>
-									<p></p>
-									<p><span class="fa fa-star checked"></p>
-									
-								</div>
-								<p class="w3-large w3-text-theme"><b><i class="fa fa-code w3-margin-right w3-text-teal"></i>Langages Informatique</b></p>
-								<div class="grid-container">
-									<p>HTML / CSS</p>								
-									<p>SQL</p>
-										<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
-									<p>PHP / JavaScript</p>								
-									<p>Python</p>
-										<p><span class="fa fa-star checked"></span></p>
-									<p>C / C++</p>								
-									<p>JAVA</p>
-										<p><span class="fa fa-star checked"></span></p>
-									<p>VB / VBA /VB.NET</p>
-									<p></p>
-										<p><span class="fa fa-star checked"></span></p>
-								</div>
-								<p class="w3-large"><b><i class="fa fa-cubes w3-margin-right w3-text-teal"></i>Environnement de développement</b></p>
-								<div class="grid-container">
-									<p>WampServer</p>
-									<p></p>
-										<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
-									<p>Eclipse</p>								
-									<p>Android Studio</p>	
-										<p><span class="fa fa-star checked"></span></p>
-								</div>
-								<p class="w3-large"><b><i class="fa fa-object-group w3-margin-right w3-text-teal"></i>Logiciels Entreprise</b></p>
-								<div class="grid-container">
-									<p>Microsoft Office</p>
-									<p></p>
-										<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
-									<p>GLPI / OCS</p>								
-									<p>Git</p>
-										<p><span class="fa fa-star checked"></span></p>
-									<p>Adobe CS6 Design</p>
-									<p></p>
-										<p><span class="fa fa-star checked"></span></p>
-								</div>
-								<p class="w3-large"><b><i class="fa fa-server w3-margin-right w3-text-teal"></i>Réseaux</b></p>
-								<div class="grid-container">
-									<p>Courant faible (VDI)</p>
-									<p>Commutateur</p>
-										<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
-									<p>Fibre optique</p>
-									<p></p>
-										<p><span class="fa fa-star checked"></span></p>										
-								</div>
+							<div class="grid-container">
+								<p>Autonome <br> <span class="fa fa-star checked"></span></p>
+								<p></p>
+								<p>Confirmé <br><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
+							</div>
+							<p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Langues</b></p>
+							<div class="grid-container">
+								<p>Français</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
+								<p> Anglais</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></p>
+							</div>
+							<p class="w3-large w3-text-theme"><b><i class="fa fa-code w3-margin-right w3-text-teal"></i>Langages Informatique</b></p>
+							<div class="grid-container">
+								<p>HTML / CSS</p>								
+								<p>SQL</p>
+								<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
+								<p>PHP / JavaScript</p>								
+								<p>Python</p>
+								<p><span class="fa fa-star checked"></span></p>
+								<p>C / C++</p>								
+								<p>JAVA</p>
+								<p><span class="fa fa-star checked"></span></p>
+								<p>VB / VBA /VB.NET</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></span></p>
+							</div>
+							<p class="w3-large"><b><i class="fa fa-cubes w3-margin-right w3-text-teal"></i>Environnement de développement</b></p>
+							<div class="grid-container">
+								<p>WampServer</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
+								<p>Eclipse</p>								
+								<p>Android Studio</p>	
+								<p><span class="fa fa-star checked"></span></p>
+							</div>
+							<p class="w3-large"><b><i class="fa fa-object-group w3-margin-right w3-text-teal"></i>Logiciels Entreprise</b></p>
+							<div class="grid-container">
+								<p>Microsoft Office</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
+								<p>GLPI / OCS</p>								
+								<p>Git</p>
+								<p><span class="fa fa-star checked"></span></p>
+								<p>Adobe CS6 Design</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></span></p>
+							</div>
+							<p class="w3-large"><b><i class="fa fa-server w3-margin-right w3-text-teal"></i>Réseaux</b></p>
+							<div class="grid-container">
+								<p>Courant faible (VDI)</p>
+								<p>Commutateur</p>
+								<p><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></p>
+								<p>Fibre optique</p>
+								<p></p>
+								<p><span class="fa fa-star checked"></span></p>										
+							</div>
 						</div>
 					</div>	<!-- End Left Column -->
 					<div class="w3-twothird">	<!-- Right Column -->
 						<div class="w3-container w3-card w3-white w3-card-4 w3-margin-bottom">
 							<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Expériences Professionnelles</h2>
 							<div class="w3-container">
-								<div style="position:relative;"><img src="./images/cv/SOGETI.png" alt="SOGETI" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/SOGETI.png" alt="SOGETI">
 								<h5 class="w3-opacity"><b>Apprenti Administrateur Réseau</b></h5>
-								<h6 class="w3-text-teal">Capgemini - Saint-Cloud / Sept 2018 - <span class="w3-tag w3-teal w3-round">Actuel</span></h6>
-								</div>
+								<h6 class="w3-text-teal">Capgemini - Saint-Cloud / Sept 2018 - <span class="w3-tag w3-teal w3-round">Actuel</span></h6>								
 								<p>-	Administration d'infrastructure réseau.<br>
-									-	Paramétrage et installation de commutateur HP.</p>								
+								-	Paramétrage et installation de commutateur HP.</p>								
 							</div>
 							<div class="w3-container">
-								<div style="position:relative;"><img src="./images/cv/CEA.png" alt="CEA" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/CEA.png" alt="CEA">
 								<h5 class="w3-opacity"><b>Apprenti Assistant à Maîtrise d'Ouvrage </b></h5>
-								<h6 class="w3-text-teal">CEA - Ile-de-France / 2017 - 2018</h6> 
-								</div>
+								<h6 class="w3-text-teal">CEA - Ile-de-France / 2017 - 2018</h6> 								
 								<p>-	Analyse du besoin des utilisateurs (interviews, analyse des procédures, …).<br>
-									-	Développement logiciel dans un outil de gestion de portefeuilles de projets dans l'outil SharePoint.<br>
-									-	Réalisation de fonctionnalités dans l'outil Excel et PowerPoint et développement de macro.</p>								
+								-	Développement logiciel dans un outil de gestion de portefeuilles de projets dans l'outil SharePoint.<br>
+								-	Réalisation de fonctionnalités dans l'outil Excel et PowerPoint et développement de macro.</p>								
 							</div>
 							<div class="w3-container">
-								<div style="position:relative;"><img src="./images/cv/G4.png" alt="G4" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/G4.png" alt="G4">
 								<h5 class="w3-opacity"><b>Apprenti Développeur</b></h5>
 								<h6 class="w3-text-teal"> G4 - Arpajon / 2015 - 2017</h6>
-								</div>
 								<p>-	Développement d’interfaces utilisateur dans un progiciel de gestion intégré Syteline.<br>
-									-	Réalisation de modules spécifiques et leur documentation selon un cahier des charges. <br>
-									-	Développement de rapports personnalisés dans un service de reporting (SSRS).</p>								
+								-	Réalisation de modules spécifiques et leur documentation selon un cahier des charges. <br>
+								-	Développement de rapports personnalisés dans un service de reporting (SSRS).</p>								
 							</div>
 							<div class="w3-container">
-								<div style="position:relative;"><img src="./images/cv/SDEL.png" alt="SDEL TC" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/SDEL.png" alt="SDEL TC">
 								<h5 class="w3-opacity"><b>Apprenti Technicien</b></h5>
 								<h6 class="w3-text-teal">SDEL Transport en Commun - Villejuif  / 2012 - 2015 </h6>
-								</div>
 								<p>-	Réalisation et dépannage d'installation VDI (Voix, Données, Images) et systèmes d’alarme incendie.<br>
-									-	Installation et branchement de fibres optiques dans des baies de brassage informatiques. <br>
-									-	Installation d’antennes relais de téléphonie mobile.</p>								
+								-	Installation et branchement de fibres optiques dans des baies de brassage informatiques. <br>
+								-	Installation d’antennes relais de téléphonie mobile.</p>								
 							</div>
 						</div>
 						<div class="w3-container w3-card w3-white w3-card-4 w3-margin-bottom">
 							<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-mortar-board w3-margin-right w3-xxlarge w3-text-teal"></i>Formations</h2>
 							<div class="w3-container">
-							<div style="position:relative;"><img src="./images/cv/IUTE.png" alt="IUT EVRY" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/IUTE.png" alt="IUT EVRY">
 								<h5 class="w3-opacity"><b>Licence Professionnelle en informatique (LP ISVD)</b></h5>
-								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">En cours</span> Université d'Evry-Val-d'Essonne - Université Paris-Saclay / 2018 - 2019 </h6>
-							</div>
+								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">En cours</span> Université d'Evry-Val-d'Essonne - Université Paris-Saclay / 2018 - 2019 </h6>							
 								<p>Intégration des Systèmes Voix Données - Métiers des réseaux informatiques et télécommunications</p>
 							</div>
 							<div class="w3-container">
-							<div style="position:relative;"><img src="./images/cv/IUTO.png" alt="IUT ORSAY" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/IUTO.png" alt="IUT ORSAY">
 								<h5 class="w3-opacity"><b>Licence Professionnelle en informatique (LP PRISM)</b></h5>
-								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">Obtenu</span> Université d'Orsay Paris-Sud - Université Paris-Saclay / 2017 - 2018 </h6>
-							</div>
+								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">Obtenu</span> Université d'Orsay Paris-Sud - Université Paris-Saclay / 2017 - 2018 </h6>							
 								<p>Programmation Internet et Systèmes Mobiles - Conception, développement et test de logiciels</p>
 							</div>
 							<div class="w3-container">
-							<div style="position:relative;"><img src="./images/cv/INSTA.png" alt="CFA INSTA" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/INSTA.png" alt="CFA INSTA">
 								<h5 class="w3-opacity"><b>Brevet de Technicien Supérieur (BTS SIO SLAM)</b></h5>
 								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">Obtenu</span> CFA INSTA Paris / 2015 - 2017 </h6>
-							</div>
-								<p>Services Informatiques aux Organisations - Solutions Logicielles et Applications Métiers</p>								
-							</div>
+								<p>Services Informatiques aux Organisations - Solutions Logicielles et Applications Métiers</p>
+							</div>				
 							<div class="w3-container">
-							<div style="position:relative;"><img src="./images/cv/FDME.png" alt="FDME Evry" style="position:absolute; right:0;height:100%;">
+								<img id="imgCV" src="./images/cv/FDME.png" alt="FDME Evry">
 								<h5 class="w3-opacity"><b>Baccalauréat Professionnel (Bac Pro ELEEC)</b></h5>
-								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">Obtenu</span> Faculté des Métiers de l'Essonne, Campus d'Évry / 2012 - 2015 </h6>
-							</div>	
+								<h6 class="w3-text-teal"><span class="w3-tag w3-teal w3-round">Obtenu</span> Faculté des Métiers de l'Essonne, Campus d'Évry / 2012 - 2015 </h6>							
 								<p>Électrotechnique, Énergie, Équipements Communicants </p>
 							</div>
 						</div>						
 					</div>	<!-- End Right Column -->				
-					<center><div class="w3-container"><!-- Bottom Column -->
+					<div class="w3-container w3-center"><!-- Bottom Column -->
 						<div class="w3-white w3-text-grey w3-card-4">
-						<div class="w3-container">
-						<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-star-o w3-margin-right w3-xxlarge w3-text-teal"></i>Centres d'intérêt</h2>
-						  <div class="w3-container w3-quarter">
-								<div class="w3-container">
-								<h5><i class="fa fa-futbol-o w3-margin-right w3-xxlarge w3-text-teal"></i><b>Sports</b></h5>								
-								<p>Équitation galop 4</p><p>Salle de sport</p><p>Course à pied</p>
-								</div>	
-						  </div>
-						  <div class="w3-container w3-quarter">
-								<div class="w3-container">
-								<h5><i class="fa fa-newspaper-o w3-margin-right w3-xxlarge w3-text-teal"></i><b>Hoobies</b></h5>								
-								<p>Actualité numérique</p><p>Electronique / Informatique</p><p>Audiophile</p><p>Piano en Autodidacte</p>
-								</div>	
-						  </div>
-						  <div class="w3-container w3-quarter">
-								<div class="w3-container">
-								<h5><i class="fa fa-gamepad w3-margin-right w3-xxlarge w3-text-teal"></i><b>Multimédia</b></h5>								
-								<p>Film et série - Animé</p><p>Livre - Light novel</p><p>Jeu vidéo - Action/Aventure</p><p>Musique - Rock/Métal</p>
-								</div>	
-						  </div>
-						  <div class="w3-container w3-quarter">
-								<div class="w3-container">
-								<h5><i class="fa fa-group w3-margin-right w3-xxlarge w3-text-teal"></i><b>Association BD3A</b></h5>								
-								<p>Organisation d’activités sportives et évènementielles contribuant à l’intégration<br>d’apprentis au sein de la Faculté des Métiers.</p>
-								</div>	
-						  </div>
-						</div><hr>
+							<div class="w3-container">
+								<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-star-o w3-margin-right w3-xxlarge w3-text-teal"></i>Centres d'intérêt</h2>
+								<div class="w3-container w3-quarter">
+									<h5><i class="fa fa-futbol-o w3-margin-right w3-xxlarge w3-text-teal"></i><b>Sports</b></h5>								
+									<p>Équitation galop 4</p><p>Salle de sport</p><p>Course à pied</p>
+								</div>
+								<div class="w3-container w3-quarter">
+									<h5><i class="fa fa-newspaper-o w3-margin-right w3-xxlarge w3-text-teal"></i><b>Hoobies</b></h5>								
+									<p>Actualité numérique</p><p>Electronique / Informatique</p><p>Audiophile</p><p>Piano en Autodidacte</p>
+								</div>
+								<div class="w3-container w3-quarter">
+									<h5><i class="fa fa-gamepad w3-margin-right w3-xxlarge w3-text-teal"></i><b>Multimédia</b></h5>								
+									<p>Film et série - Animé</p><p>Livre - Light novel</p><p>Jeu vidéo - Action/Aventure</p><p>Musique - Rock/Métal</p>
+								</div>
+								<div class="w3-container w3-quarter">
+									<h5><i class="fa fa-group w3-margin-right w3-xxlarge w3-text-teal"></i><b>Association BD3A</b></h5>								
+									<p>Organisation d’activités sportives et évènementielles contribuant à l’intégration<br>d’apprentis au sein de la Faculté des Métiers.</p>
+								</div>
+							</div><hr>
 						</div>
-					</div><center><!-- End Bottom Column -->	
+					</div><!-- End Bottom Column -->	
 				</div>	<!-- End Grid -->			
 			</div><br>	<!-- End Page Container -->
-			<!--<span class="glyphicon glyphicon-save text-center" style="font-size:20px; color:white;">Télécharger</span>-->
+		<!--<span class="glyphicon glyphicon-save text-center" style="font-size:20px; color:white;">Télécharger</span>-->
 		</div>
